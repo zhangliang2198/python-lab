df022b5ec94dcf6d215b3eeef8f94c80401e48c0
mikoozhang <48906088@qq.com>
2023-03-15 09:51:07 +0800

    调整位置

---
+++ base/base3.py
@@ -0,0 +1,21 @@
+import keyword
+
+print(keyword.kwlist)
+print("-" * 50)
+'''
+多行注释
+多行注释
+'''
+
+"""
+多行注释
+多行注释
+"""
+
+a = 1
+b = 2
+print(a)
+print(b)
+# 不换行输出
+print(a, end=" ")
+print(b, end=" ")
---
+++ base/base7/main.py
@@ -0,0 +1,69 @@
+import p
+
+# 切片操作
+num_str = "0123456789"
+
+# 1. 截取从 2 ~ 5 位置 的字符串
+print(num_str[2:6])
+
+# 2. 截取从 2 ~ `末尾` 的字符串
+print(num_str[2:])
+
+# 3. 截取从 `开始` ~ 5 位置 的字符串
+print(num_str[:6])
+
+# 4. 截取完整的字符串
+print(num_str[:])
+
+# 5. 从开始位置，每隔一个字符截取字符串
+print(num_str[::2])
+
+# 6. 从索引 1 开始，每隔一个取一个
+print(num_str[1::2])
+
+# 倒序切片
+# -1 表示倒数第一个字符
+print(num_str[-1])
+
+# 7. 截取从 2 ~ `末尾 - 1` 的字符串
+print(num_str[2:-1])
+
+# 8. 截取字符串末尾两个字符
+print(num_str[-2:])
+
+# 9. 字符串的逆序（面试题）
+print(num_str[::-1])
+
+'''
+字典遍历
+'''
+students = [
+    {"name": "阿土",
+     "age": 20,
+     "gender": True,
+     "height": 1.7,
+     "weight": 75.0},
+    {"name": "小美",
+     "age": 19,
+     "gender": False,
+     "height": 1.6,
+     "weight": 45.0},
+]
+
+find_name = "阿土"
+
+for stu_dict in students:
+
+    print(stu_dict)
+
+    # 判断当前遍历的字典中姓名是否为find_name
+    if stu_dict["name"] == find_name:
+        print("找到了")
+
+        # 如果已经找到，直接退出循环，就不需要再对后续的数据进行比较
+        break
+
+else:
+    print("没有找到")
+
+print("循环结束")
