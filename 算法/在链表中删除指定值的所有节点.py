2d939ac50b3b8bf6e348dc8658cef1c6c9f42797
mikoozhang <48906088@qq.com>
2023-03-23 16:54:14 +0800

    完成

--- 算法/在链表中删除指定值的所有节点.py
+++ 算法/在链表中删除指定值的所有节点.py
@@ -1,96 +1,59 @@
-from typing import Any
+class ListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
 
-class SingleNode:
-    def __init__(self, data: int, next_node):
-        self.data = data
-        self.next_node = next_node
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
+    def append(self, value):
+        new_node = ListNode(value)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
 
-class DoubleNode:
-    def __init__(self, data, pre_node, next_node):
-        self.date = data
-        self.next_node = next_node
-        self.pre_node = pre_node
+    def remove(self, value):
+        if not self.head:
+            return
 
+        while self.head.value == value:
+            self.head = self.head.next
 
-def make_node() -> SingleNode:
-    # 创建一些node
-    node5 = SingleNode(4, None)
-    node4 = SingleNode(3, node5)
-    node3 = SingleNode(2, node4)
-    node2 = SingleNode(1, node3)
-    node1 = SingleNode(0, node2)
-    return node1
+        current = self.head
+        while current.next:
+            if current.next.value == value:
+                current.next = current.next.next
+            current = current.next
 
-
-def delete_node(head_node: SingleNode, delete_node_s: SingleNode) -> SingleNode | None:
-    if head_node.next_node is None:
-        # 如果只有一个节点
-        if head_node == delete_node_s:
-            return None
-        else:
-            return head_node
-    if head_node == delete_node_s:
-        # 当要删除的节点等于头节点的时候，则返回第二个节点
-        res = head_node.next_node
-        # 删除第一个节点
-        head_node.next_node = None
-        del head_node
-        return res
-    # 开始正片
-    o_head = head_node
-    while head_node:
-        org = head_node.next_node
-        if delete_node_s == head_node.next_node:
-            head_node.next_node = head_node.next_node.next_node
-            org.next_node = None
-            del org
-        head_node = head_node.next_node
-    return o_head
-
-
-def delete_value(head_node: SingleNode, param: int) -> SingleNode | None:
-    if head_node.next_node is None:
-        if head_node.data == param:
-            return None
-        else:
-            return head_node
-    # 删除指定值的所有节点
-    r_head = head_node
-    pre = head_node
-    curr = head_node.next_node
-    while curr:
-        if pre.data == param:
-            r_head = curr
-            pre = curr
-            curr = curr.next_node
-        else:
-            if curr.data == param:
-                pre.next_node = curr.next_node
-                curr = curr.next_node
-            else:
-                pre = pre.next_node
-                curr = curr.next_node
-    return r_head
+    def display(self):
+        current = self.head
+        while current:
+            print(current.value, end=' -> ')
+            current = current.next
+        print("None")
 
 
 if __name__ == "__main__":
-    # 创建一些node
-    node1 = make_node()
-    while node1:
-        print(node1.data)
-        node1 = node1.next_node
+    # 示例用法：
+    linked_list = LinkedList()
+    linked_list.append(1)
+    linked_list.append(2)
+    linked_list.append(3)
+    linked_list.append(4)
 
-    node1 = make_node()
-    head = delete_node(node1, node1.next_node)
+    print("原始链表：")
+    linked_list.display()
 
-    while head:
-        print(head.data)
-        head = head.next_node
+    linked_list.remove(3)
+    print("删除元素 3 后的链表：")
+    linked_list.display()
 
-    node1 = make_node()
-    head_node = delete_value(node1, 4)
-    while head_node:
-        print(head_node.data)
-        head_node = head_node.next_node
+    linked_list.remove(1)
+    print("删除元素 1 后的链表：")
+    linked_list.display()
